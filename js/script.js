51f56eae37c0b69e4f1e218399dfc43f38938985
MalarkeyKazoo <6891850+malarkeykazoo@users.noreply.github.com>
2021-03-09 18:42:39 -0600

    Added JS for dynamic display fields

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 function init(){
   window.addEventListener('load', init);
+  document.getElementById("phoneContact").addEventListener("click", showPhone); 
+  document.getElementById("specialReq").addEventListener("click", showSpec);
+  document.getElementById("acceptTC").addEventListener("click", showSubmit);
   function showPhone(){
-    
+    var phoneCheck = document.getElementById("phoneContact");
+    var phoneIn = document.getElementById("phoneSection")
+    if (phoneCheck.checked == true){
+      phoneIn.style.display = "block";
+    } else {
+      phoneIn.style.display = "none";
+    }
+  }
+  function showSpec(){
+    var specCheck = document.getElementById("specialReq");
+    var specIn = document.getElementById("requestSection")
+    if (specCheck.checked == true){
+      specIn.style.display = "block";
+    } else {
+      specIn.style.display = "none";
+    }
+  }
+  function showSubmit(){
+    var subCheck = document.getElementById("acceptTC");
+    var subIn = document.getElementById("submitContain")
+    if (subCheck.checked == true){
+      subIn.style.display = "block";
+    } else {
+      subIn.style.display = "none";
+    }
   }
 }
